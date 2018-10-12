e72251252b686ebc6d5cbcb64b28b7c1245dc563
shuviru219 <shuvam.jha007@gmail.com>
2018-10-12 19:33:10 +0530

    Update test_jenkins.py

--- test_jenkins.py
+++ test_jenkins.py
@@ -2,5 +2,5 @@ def lambda_handler(event, context):
     a=1
     b=3
     c=a+b
-    print "Invokedsssssssssss Lambda",c
-    return "Invokedssssssss Lambda",c
+    print "Invoked by Jenkins",c
+    return "Invoked by Jenkins",c
